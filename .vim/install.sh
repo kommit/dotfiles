d7aff3597ba2f735099f53556a15c15d0809ba0d
kommit <kommit@gmail.com>
2018-09-19 17:12:06 +0800

    Vundle => Vim-Plugs

--- .vim/install.sh
+++ .vim/install.sh
@@ -7,4 +7,9 @@ mkdir -p backup
 mkdir -p swap
 mkdir -p bundle
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Install Vim-Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install Vundle
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
